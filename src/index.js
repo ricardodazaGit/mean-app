682daa0eaca9e3f5e4ebe9c3ba37ba164e1747f5
Ricardo Daza <ricardo.daza@outlook.com>
2018-05-21 16:12:04 +0200

    feat: hello world from routes/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("mean-app")
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'));
+
+app.listen(app.get('port'), () =>{
+    console.log('server on port', app.get('port'))
+});
