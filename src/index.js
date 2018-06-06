ccfdce0f8524f9dc9e82743f8e2d63d363fb84aa
Ricardo Daza <ricardo.daza@outlook.com>
2018-06-06 15:39:17 +0200

    asdas

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ Object.assign=require('object-assign')
 //settings
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-//app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('port', port);
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 //app.set('view engine', 'ejs');
