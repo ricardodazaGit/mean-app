4a526d541593e7be8fd447527d79492dfcaca4bc
Ricardo Daza <ricardo.daza@outlook.com>
2018-06-06 16:28:49 +0200

    asdas

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     mongoURLLabel = "";
 
 app.get('/', (req, res) => {
-  res.render('index.html', { pageCountMessage : null});
+  res.render('server.html', { pageCountMessage : null});
 });
 
 app.get('/pagecount', function (req, res) {
